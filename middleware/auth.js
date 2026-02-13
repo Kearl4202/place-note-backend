3f4009f0b1ba1ee1504bc3082317300d0cf33492
Kearl4202 <kearl4202@gmail.com>
2026-02-13 11:10:39 -0600

    Fix auth.js syntax error

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+  const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
     return res.status(401).json({ error: 'Access token required' });
@@ -17,4 +17,4 @@ function authenticateToken(req, res, next) {
   });
 }
 
-module.exports = { authenticateToken };re;
+module.exports = { authenticateToken };
