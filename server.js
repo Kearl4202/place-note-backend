109ed2936253712181978d579cae095998b2262b
Kearl4202 <kearl4202@gmail.com>
2026-02-27 20:20:49 -0600

    server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
+const locationRoutes = require('./routes/location');
 
 const authRoutes = require('./routes/auth');
 const subscriptionRoutes = require('./routes/subscriptions');
@@ -21,6 +22,7 @@ app.use('/api/contacts', contactRoutes);
 app.use('/api/groups', groupRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/chat', chatRoutes);
+app.use('/api/location', locationRoutes);
 
 app.get('/health', (req, res) => {
   res.json({ status: 'OK', message: 'Place Note API is running' });
